1572b1653d5228423bb861108b3bd559b68cf6f5
Jefferson Junior <105082760+jeffersonnjunior@users.noreply.github.com>
2026-08-07 14:41:07 -0300

    fix: delete folder

--- credit_engine/services/proposal.py
+++
@@ -1,7 +0,0 @@
-from credit_engine.models.proposal import CreditDecision, ProposalCreate
-from credit_engine.services import risk as risk_service
-
-
-async def create_proposal(payload: ProposalCreate) -> CreditDecision:
-    """Create/evaluate a credit proposal (in-memory for now)."""
-    return await risk_service.evaluate_proposal(payload)
