89649d1ad102c41b4a18d700ac786c03f7bbf6e3
Jefferson Junior <105082760+jeffersonnjunior@users.noreply.github.com>
2026-08-07 14:43:00 -0300

    fix: delete folder

--- credit_engine/api/v1/__init__.py
+++
@@ -1,6 +0,0 @@
-from fastapi import APIRouter
-
-from credit_engine.api.v1.proposals import proposals_router
-
-v1_router = APIRouter()
-v1_router.include_router(proposals_router, prefix="/proposals", tags=["proposals"])
